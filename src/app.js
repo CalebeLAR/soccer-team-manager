454bbe99547615a6705aceef62d80d7572d3a876
CalebeLAR <ca6ele@gmail.com>
2023-02-26 15:34:06 -0400

    refat

--- src/app.js
+++ src/app.js
@@ -2,4 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+// rotas
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
 module.exports = app;
