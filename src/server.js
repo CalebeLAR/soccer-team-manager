51997c8a4151f624c4dfc29a9a37b8432255d842
CalebeLAR <ca6ele@gmail.com>
2023-02-26 16:03:09 -0400

    END POINST

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const PORT = 3000;
 
-app.listen(3000, () => console.log(`Aplicação rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`));
 
